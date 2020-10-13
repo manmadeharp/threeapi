dff273e92f935dd03c474087f1fcf7a4975c9d3a
Finbar <finbarccowan@gmail.com>
2020-10-13 14:15:57 +0100

    third commit

--- index.js
+++ index.js
@@ -7,11 +7,69 @@ const getPokemon = require('./lib/getPokemon');
 const getSwapi = require('./lib/getSwapi');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index'); 
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
 app.engine('.hbs', hbs({
     extname: '.hbs',
     defaultLayout: 'layout'
 }));
 
-app.listen(3000, () =>  {
+app.set('view engine', '.hbs');
 
+app.post('/harry', async (req, res) =>{
+    let harrydata = await getHarry();
+    if (harrydata.cod == 404) {
+        res.render('index', {err: 'location wasn\'t found'});
+    } 
+    else {
+        let harrytest = harrydata.url;
+        console.log(Harrytest)
+        res.render('index', {
+            harrytest
+        })}
+    
+})
+app.post('/pokemon', async (req, res) => {
+    let pokemondata = await getPokemon();
+    if (pokemondata.cod == 404) {
+        res.render('index', {err: 'location wasn\'t found'});
+    } 
+    else {
+        let pokemontest = pokemondata.ability
+        console.log(pokemontest)
+        res.render('index', {
+            pokemontest
+        })}
+
+    if (swapidata.cod == 404) {
+        res.render('index', {err: 'location wasn\'t found'});
+    } 
+    else {
+        let swapitest = swapidata.people
+        res.render('index', {
+            swapitest
+    })}
+
+})
+app.post('/starwars', async (req, res) => {
+    let swapidata = await getSwapi();
+    if (swapidata.cod == 404) {
+        res.render('index', {err: 'location wasn\'t found'});
+    } else {
+        let swapitest = swapidata.people
+        console.log(swapitest)
+        res.render('index', {
+            swapitest
+    })}
+})
+app.listen(3000, () =>  {
+    console.log("listening on port 3000")
 })
